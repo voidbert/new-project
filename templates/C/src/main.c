cdd6c508869c113bada3b454f9502122c68bf8b1
voidbert <50591320+voidbert@users.noreply.github.com>
2023-09-11 14:47:42 +0000

    C template: add doxygen

--- templates/C/src/main.c
+++ templates/C/src/main.c
@@ -1,6 +1,11 @@
 /* %LICENSE_HEADER% */
 #include <stdio.h>
 
+/**
+ * @brief The entry point to the program.
+ * @retval 0 Success
+ * @retval 1 Insuccess
+ */
 int main(void) {
 	puts("Hello, world!");
 	return 0;
